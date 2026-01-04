1325d15a27ee19d84bac7b2afbe8e8e52204728e
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-01-04 23:16:44 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (13.26%)

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,11 +4,11 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
- int n= nums.size();
- int sum = (n*(n+1)/2);
+ int n= nums.size();  // n stores largest element/last element of vector array nums
+ int sum = (n*(n+1)/2);   // sum of n numbers from arithmetic progression 
 for(int x : nums)        
         {
-            sum = sum-x;
+            sum = sum-x;     // we are deleting actual numbers of nums vector array from expected sum so at last we get missing number
         }
       return sum;
   }
