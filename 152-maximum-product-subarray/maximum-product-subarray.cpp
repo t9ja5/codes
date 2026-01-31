fd555bbccfcdd2979d807dfab7424e46440b34d6
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-01-31 12:55:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.63%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+       int maxprod=nums[0];
+       int minprod =nums[0];
+       int ans=nums[0];
+
+       for(int i=1 ; i<nums.size(); i++){
+        if(nums[i]<0) swap(maxprod,minprod);
+
+        maxprod = max(nums[i], maxprod*nums[i]);
+        minprod = min(nums[i],minprod*nums[i]);
+    ans =max(ans,maxprod);
+       }
+return ans;
+    }
+};
