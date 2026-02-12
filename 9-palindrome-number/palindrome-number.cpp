410ae7c0caf645f66c14a11f3fda4560fe1f1e6d
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-02-12 10:39:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.75%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        int tempx=x;
         if(x<0) return false;
-        long long int res=0;
-        int og=x;
-        while(x!=0){
-            int ld= x%10;
-            x=x/10;
-            res=res*10+ld;
-        }
-        if(res==og){
-            return true;
-
-
-        }
-    return false;
+        long long int rev=0,lastdigit=0;
+        while(tempx!=0){
+            lastdigit = tempx%10;
+            rev=rev*10+lastdigit;
+            tempx=tempx/10;
+             }
+             if(rev==x) return true;
+            else return false;
     }
 };
